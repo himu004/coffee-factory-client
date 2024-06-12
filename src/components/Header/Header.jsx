e8e68013b42c5505de0b36b878e548e2976017db
Himu004 <himu.chowdhury01@gmail.com>
2024-06-12 16:49:11 +0600

    Banner Section Completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,17 @@ import headerImg from "../../assets/images/more/15.jpg";
 import logo from "../../assets/images/more/logo1.png";
 
 const Header = () => {
-    return (
-        <div style={{ backgroundImage: `url(${headerImg})` }} className="bg-cover bg-center h-[120px] flex items-center justify-center">
-            <img className="w[75px] h-[90px]" src={logo} alt="" />
-            <h1 className="text-white md:text-5xl">Coffee Factory</h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${headerImg}`,
+      }}
+      className="bg-cover bg-center h-[110px] flex items-center justify-center"
+    >
+      <img className="w[75px] h-[90px]" src={logo} alt="" />
+      <h1 className="text-white md:text-5xl">Coffee Factory</h1>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
-
+import bgImage from '../../assets/images/more/6.jpeg';
 const Home = () => {
     return (
-        <div>
-            <h1 className="text-center text-2xl">Coffe Factory Project Client</h1>
+        <div style={{ backgroundImage: `url(${bgImage})` }} className="bg-cover bg-center h-screen flex items-center -scale-x-100">
+            {/* Banner Have to move on external components */}
+            <div className='w-[724px] text-white -scale-x-100 flex flex-col justify-items-start space-y-5'>
+                <h2 className='text-4xl'>Would you like a Cup of Delicious Coffee?</h2>
+                <p className='text-gray-400'>
+                Its coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.
+                </p>
+                <button className='text-[#242222] border border-transparent w-[130px] bg-[#E3B577] py-3 transition hover:text-white hover:border hover:border-white hover:bg-transparent'>Learn More</button>
+            </div>
+            
         </div>
     );
 };
